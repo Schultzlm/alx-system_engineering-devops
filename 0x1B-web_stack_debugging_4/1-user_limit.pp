997b52a29b843fccef3873bdb690dd3097f800ca
schultzlm <litokwas@programmer.net>
2023-06-15 08:52:54 +0000

    web_stack-4

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,3 +1,4 @@
 # Here I will fix the user limit
-exec { "sed -i -E '56s/[0-9]+/500/;57s/[0-9]+500/' /etc/security/limits.conf":
+exec { "sed -i -E '56s/[0-9]+/500/;57s/[0-9]+/500/' /etc/security/limits.conf":
 path => [ '/bin/', '/sbin/' , '/usr/bin/', '/usr/sbin/' ]
+}
